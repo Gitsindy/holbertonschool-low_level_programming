f0c684de3ce694e78a29c528295959b99c8f6b94
Gitsindy <sindysilva520@gmail.com>
2022-06-22 15:12:02 -0700

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,3 @@
 #!/bin/bash
-gcc  puts ("Programming is like building a multilingual puzzle")
+gcc  puts -Wall -Werror -Wextra -pedantic -std=gnu89 4-puts.c && ./a.out
+"Programming is like building a multilingual puzzle"
