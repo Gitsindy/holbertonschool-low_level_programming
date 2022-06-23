c99bb8f600ec1180b96c6ab969e7291a4c3d666c
Gitsindy <sindysilva520@gmail.com>
2022-06-23 14:15:03 -0700

    6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - Entry point, use size 
+ *main - Entry point, use size
  *
- * Return: 0
+ *Return: 0
  */
-int main(int argc, char *argv[])
+int main(void)
 {
-	printf("size of a char: 1 byte\n",sizeof(char));
-	printf("Size of an int: 4 byte\n",sizeof(int));
-	printf("Size of a long int: 8 byte\n",sizeof(long));
-	printf("Size of a long long int: 8 byte\n",sizeof(long long));
-	printf("Size of a float: 4 byte\n",sizeof(float));
-	return(0);
+	printf("size of a char: 1 byte\n", sizeof(char));
+	printf("Size of an int: 4 byte\n", sizeof(int));
+	printf("Size of a long int: 8 byte\n", sizeof(long));
+	printf("Size of a long long int: 8 byte\n", sizeof(long long));
+	printf("Size of a float: 4 byte\n", sizeof(float));
+	return (0);
 }
