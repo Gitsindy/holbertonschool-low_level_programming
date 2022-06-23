def39d307c8c2a67aeea90085781ec7fa24bbc31
Gitsindy <sindysilva520@gmail.com>
2022-06-23 11:16:04 -0700

    6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,12 +7,10 @@
  */
 int main(int argc, char *argv[])
 {
-	printf ("size of a char: 1 bytes", sizeof (char));
-	printf ("Size of an int: 4 bytes", sizeof (int));
-	printf ("Size of a long int: 8 bytes", sizeof (long));
-	printf ("Size of a long long int: 8 bytes", sizeof (long long));
-	printf ("Size of a float: 4 bytes", sizeof (float));
+	printf ("size of a char: 1 byte" (char));
+	printf ("Size of an int: 4 byte"(int));
+	printf ("Size of a long int: 8 byte"(long));
+	printf ("Size of a long long int: 8 byte"(long long));
+	printf ("Size of a float: 4 byte"(float));
 	return (0);
 }
-	 
-
