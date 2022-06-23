fc72c21d6d1e45100c5580d8cefc565dbf6d2d54
Gitsindy <sindysilva520@gmail.com>
2022-06-23 07:46:17 -0700

    0-hello_world

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,8 @@
+#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-int main(){
+int main() {
  char cisfun[50];
- puts("Programming is like building a multilingual puzzle");
+ puts("\"Programming is like building a multilingual puzzle");
  puts(cisfun);
  return 0;
 }
