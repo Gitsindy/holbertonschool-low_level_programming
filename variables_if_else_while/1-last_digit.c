b276f74fe2af98e064af507b7b0a599d5083466e
Gitsindy <sindysilva520@gmail.com>
2022-06-24 20:25:38 -0500

    1-last_digit.c

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * *main -  print whether the number is positive or negative.
+ * *
+ * * Return: positive, zero, negative
+ */
+int main(void)
+{
+	int n = 0;
+	
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("98 is positive\n");
+	}
+	else if (n < 0)
+	{
+		printf("-98 is negative\n");
+	}
+	else
+	{
+		printf("0 is zero\n");
+	}
+
+	return (0);
+}
