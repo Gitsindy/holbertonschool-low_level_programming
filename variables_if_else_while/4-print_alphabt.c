8d1ed0e30f037c5346d347f7980a4efa1188b583
Gitsindy <sindysilva520@gmail.com>
2022-06-26 16:48:10 -0500

    4-print_alphabt.c

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	char letras = 97;
 
-	while (letras <= 122)
+	while (letras <= 122);
 	{
 		if (letras == 101)
 		{
@@ -17,7 +17,7 @@ int main(void)
 		}
 		if (letras == 113)
 		{
-			letras ++;
+			letras++;
 		}
 
 		putchar(letras);
