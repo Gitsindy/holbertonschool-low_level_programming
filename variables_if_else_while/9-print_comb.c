e0028743d6818070b62d98d1a5d8f4d018fb12fd
Gitsindy <sindysilva520@gmail.com>
2022-06-27 17:55:33 -0500

    9-print_comb.c

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -2,9 +2,11 @@
 #include <stdlib.h>
 int main(void)
 {
-	int n = 9, i, s = 1;
+	int n = 0;
+	int s = 1;
+	int i = 1;
 
-	for (i = 1; i <= n; i++)
+	for (i = 1; n <= 9; i++)
 	{
 		s = s * i;
 		putchar(s);
