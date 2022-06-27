3396a2e77fc1432d2b56e3a9bc0dd3ca8e13b01a
Gitsindy <sindysilva520@gmail.com>
2022-06-27 10:43:39 -0500

    7-print_tebahpla.c

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * *main - Write a program that prints the alphabet in lowercase .
+ * *
+ * * Return: letras
+ */
+int main(void)
+{
+	char letra = 'z';
+
+	do {
+		putchar(letra);
+		letra++;
+	} while (letra <= 'a');
+	putchar('\n');
+	return (0);
+}
