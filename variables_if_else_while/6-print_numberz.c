e7927aa9f39c7e234c6a7c0ca79db66e21cd29a5
Gitsindy <sindysilva520@gmail.com>
2022-06-26 20:27:16 -0500

    6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * *main - Write a program that prints the alphabet in lowercase .
+ * *
+ * * Return: letras
+ */
+int main(void)
+{
+	int number = 0;
+
+	while (number < 11)
+	{
+
+		putchar("%d", number);
+		number = number + 1;
+	}
+	return (0);
+}
