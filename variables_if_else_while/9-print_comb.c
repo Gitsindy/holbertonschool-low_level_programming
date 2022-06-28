c4c5110d404b240a54ce073906d064eb73e44f53
Gitsindy <sindysilva520@gmail.com>
2022-06-28 12:16:30 -0500

    9-print_comb.c

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -11,15 +11,13 @@ int main(void)
 	for (i = 48; i <= 57; i++)
 
 	{
-		if (i==57)
-		{
-			i='\n';
-		}
-		
-    
 		putchar(i);
 		putchar(',');
 		putchar(32);
+		if (i==57)
+		{
+			i='\0';
+		}
 		}
 	putchar('\n');
 	return (0);
