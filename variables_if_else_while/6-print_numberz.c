1cc2211940de1ac97e1f1474d5f9d0a1f03f2259
Gitsindy <sindysilva520@gmail.com>
2022-06-27 19:58:37 -0500

    9-print_comb.c

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -8,13 +8,10 @@
 int main(void)
 {
 	int num;
-for (num=48;num<=58;num++)
+	for (num = 48; num <= 58; num++)
 	{
-		putchar(num)
-		
-		
-		
+		putchar(num);
 	}
 	putchar('\n');
-	return (0) ;
+	return (0);
 }
