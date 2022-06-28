7e110d04a533b647a80b34c0a8bcf8cdb09d8be1
Gitsindy <sindysilva520@gmail.com>
2022-06-28 11:37:38 -0500

    3-print_alphabets.c

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -3,13 +3,18 @@ int main(void)
 {
 	int i;
 
-	for (i = 48; i <= 57; i++ )
+	for (i = 48; i <= 57; i++)
 
 	{
 
 		putchar(i);
 		putchar(',');
 		putchar(32);
+		if (i==57 i=-44)
+		{
+			i++;
+		}
+		
 	}
 	putchar('\n');
 	return (0);
