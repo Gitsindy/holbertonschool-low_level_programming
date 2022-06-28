252234d18102ef7f0c4ba5addb97fe872c27d508
Gitsindy <sindysilva520@gmail.com>
2022-06-28 08:25:28 -0500

    3-print_alphabets.c

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -7,19 +7,18 @@
  */
 int main(void)
 {
-	char min=97;
-	char myu =65;
+	char min = 97;
+	char myu = 65;
 
-	while (min <=122)
+	while (min <= 122)
 	{
 		putchar(min);
 		min = min + 1;
-		while (myu<90)
-		{ 
-			putchar(myu);
-			myu = myu +1;
-		}
-		
+	}
+	while (myu < 90)
+	{
+		putchar(myu);
+		myu = myu + 1;
 	}
 	return (0);
 }
